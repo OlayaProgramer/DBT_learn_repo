4bc421674946fd28e4e536af305e669f2f2604f6
Juan Olaya <juandavid.olaya@globant.com>
2024-07-30 11:26:55 -0500

    Made the first transformations with DBT and exercise of rename columns

---
+++ models/src/src_listings.sql
@@ -0,0 +1,18 @@
+WITH raw_listings AS (
+    SELECT * FROM AIRBNB.RAW.RAW_LISTINGS
+)
+
+SELECT 
+    id AS listing_id,
+    name AS listing_name,
+    listing_url,
+    room_type,
+    minimum_nights,
+    host_id,
+    price AS price_str,
+    created_at,
+    updated_at
+FROM
+    raw_listings
+
+-- important not put semicolons in this sentence because them will be generated problems later when you run this command "dbt run"
---
+++ models/src/src_reviews.sql
@@ -0,0 +1,15 @@
+WITH src_reviews AS (
+    SELECT 
+        *
+    FROM 
+        AIRBNB.RAW.RAW_REVIEWS 
+)
+
+SELECT
+    listing_id,
+    date AS review_date,
+    reviewer_name,
+    comments AS review_text,
+    sentiment AS review_sentiment
+FROM 
+    src_reviews
